45c2646da821b2573b16d60bec3259bc28b33644
Luis Gerónimo <lgeronimo@na-at.com.mx>
2021-12-09 16:37:14 -0600

    feat: demo completed

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ window.onload = function () {
 
 // events available
 const EVENT_MODULE = {
-  PROCESS_INIT: "INIT_MODULE",
+  INIT_MODULE: "INIT_MODULE",
+  PROCESS_INIT: "PROCESS_INIT",
   PROCESS_ERROR: "PROCESS_ERROR",
   PROCESS_COMPLETED: "PROCESS_COMPLETED",
-}
+};
 
-// acuant credentials 
+// acuant credentials
 const CREDENTIALS = {
   passiveUsername: 'acuantEUUser@naat.com',
   passivePassword: 'Q^59zWJzZ^jZrw^q',
@@ -18,24 +19,38 @@ const CREDENTIALS = {
   assureidEndpoint: "https://eu.assureid.acuant.net"
 }
 
-
-
 // legends of module
 const LEGENDS = {
-  initializing: 'iniciando',
-  processing: 'procesando',
+  initializing: "iniciando",
+  processing: "procesando",
   scan: {
-    none: 'ENFOCA TU ID SOBRE LA GUÍA',
-    smallDocument: 'ACERCATE MÁS',
-    goodDocument: '',
-    capturing: 'CAPTURANDO ',
-    tapToCapture: 'TOCA LA PANTALLA PARA CAPTURAR',
+    none: "ENFOCA TU ID SOBRE LA GUÍA",
+    smallDocument: "ACERCATE MÁS",
+    goodDocument: "",
+    capturing: "CAPTURANDO ",
+    tapToCapture: "TOCA LA PANTALLA PARA CAPTURAR",
   },
   manualCapture: {
-    instruction: 'Captura el frente de tu identificación',
-    buttonNext: 'Continuar'
-  }
+    instruction: "Captura el frente de tu identificación",
+    buttonNext: "Continuar",
+  },
 };
+
+// errors
+const ERROR_CODE = {
+  REQUIRED_CREDENTIAL: -1,
+  FAIL_INITIALIZATION: -2,
+  UNSUPPORTED_CAMERA: -3,
+  FAIL_INITIALIZATION_CAMERA_U: -4,
+  FAIL_CREATION_INSTANCE_DOCUMEN: -5,
+  FAIL_UPLOAD_IMAG: -6,
+  FAIL_GET_OC: -7,
+  FAIL_GET_FACE_IMAG: -8,
+  FACE_IMAGE_URL_NOT_FOUND: -9,
+  FACE_IMAGE_NOT_FOUN: -10,
+  RESOURCES_COULD_NOT_BE_LOADED: -11,
+  DOMAIN_NOT_ALLOWED: -12
+}
 
 
 class ResponseEvent {
@@ -47,55 +62,68 @@ class ResponseEvent {
   }
 }
 
+class Result {
+  id; // image of identification (image.data) and relevant information (sharpness, glare) 
+  idData; // ocr idData.ocr;
+  idPhoto; // image of the face cutout
+}
+
+
 // subscribe to message event to recive the events from the iframe
 window.addEventListener("message", (message) => {
   // IMPORTANT: check the origin of the data
-  // if (message.origin.includes("firmaautografa.com")) {
-    if (message.data.event === EVENT_MODULE.PROCESS_INIT) {
+  if (message.origin.includes("firmaautografa.com")) {
+    if (message.data.event === EVENT_MODULE.PROCESS_INIT) { // PROCESS_INIT
       // only informative
       console.log("Process init");
-    } else if (message.data.event === EVENT_MODULE.PROCESS_ERROR) {
-      // restart component and send error
-      alert(JSON.stringify(message.data.data));
-    } else if (message.data.event === EVENT_MODULE.PROCESS_COMPLETED) {
-      // show result
+    } else if (message.data.event === EVENT_MODULE.PROCESS_ERROR) { // PRROCESS_ERROR
+      console.log(message.data.data);
+      if (message.data.data.code === ERROR_CODE.UNSUPPORTED_CAMERA) {
+        // do something
+        alert('Cámara no soportada, intenta en otro dispositivo')
+      } else if (message.data.data.code === ERROR_CODE.FAIL_INITIALIZATIO) {
+        // restart component
+      } else {
+        // restart component
+        alert (JSON.stringify(message.data.data))
+      }
+    } else if (message.data.event === EVENT_MODULE.PROCESS_COMPLETED) { // PROCESS_COMPLETED
+      console.log("Process completed");
+      // use the results as you see fit 
+      console.log(message.data.data);
+      // show result example
       const containerResult = document.getElementById('container-result');
       const containerIframe = document.getElementById('container-iframe-acuant');
       const imageId = document.getElementById('image-id');
       const imageFace = document.getElementById('image-face');
       const ocr = document.getElementById('ocr');
-      
       containerIframe.style.display = 'none';
       containerResult.style.display = 'flex';
       imageId.src = message.data.data.id.image.data;
       imageFace.src = message.data.data.idPhoto;
       ocr.innerHTML = JSON.stringify(message.data.data.idData.ocr);
-      
-      console.log("Process completed");
     }
-  // } else return;
+  } else return;
 });
 
-
 function initIframe() {
-  // get iframe 
-  const iframe = document.getElementById('fad-iframe-acuant');
-  // url of fad, uat - uatwebfad4.firmaautografa.com, prod - mobile.firmaautografa.com
-  const url = 'https://192.168.100.7:4300';
+  // get iframe
+  const iframe = document.getElementById("fad-iframe-acuant");
+  // url - https://apiiduat.firmaautografa.com/
+  const url = "https://apiiduat.firmaautografa.com/";
   // set src to iframe
   iframe.src = url;
-  // send credentials to iframe
+  // subscribe to onload
   iframe.onload = () => {
-    iframe.contentWindow.postMessage(new ResponseEvent(EVENT_MODULE.PROCESS_INIT, {
-      credentials: CREDENTIALS,
-      legends: LEGENDS,
-      side: 0, // 0 - front id, 1 - back id
-      idPhoto: true, // true - get imaghen face of id, false - nothing
-      idData: true, // true - ocr, false - nothing
-      imageQuality: 1 // quality of image id, range 0 - 1
-    }
-  ), iframe.src);
+    // send configuration
+    iframe.contentWindow.postMessage(
+      new ResponseEvent(EVENT_MODULE.INIT_MODULE, {
+        credentials: CREDENTIALS,
+        legends: LEGENDS,
+        side: 0, // 0 - front id, 1 - back id
+        idData: true, // true - ocr, false - without this data
+        idPhoto: true, // true - get imaghen face of id, false - without this data
+        imageQuality: 1, // quality of image id, range 0 - 1
+      }), iframe.src);
   };
 }
-
-
