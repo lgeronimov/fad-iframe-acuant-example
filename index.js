9fe700521a90227aa6161c6543b476c7cbbcc884
Luis Gerónimo <lgeronimo@na-at.com.mx>
2022-05-10 14:32:54 -0500

    feat: update configuration

--- index.js
+++ index.js
@@ -87,8 +87,7 @@ const CONFIGURATION = {
       legendsInstructions: {
         title: 'Identificación',
         subtitle: 'Captura tu identifcación',
-        buttonNext: 'Continuar',
-        instructions: 'Recuerda no hacer uso de lentes de sol, gorras u otros elementos que dificulten la identificación de tu rostro.'
+        buttonNext: 'Continuar'
       },
       legendsPreview: {
         title: 'Identificación',
@@ -100,7 +99,7 @@ const CONFIGURATION = {
     }
   },
   pathDependencies: {
-    imageDirectory: 'ASSETS_URL'
+    // imageDirectory: 'ASSETS_URL'
   }
 };
 
